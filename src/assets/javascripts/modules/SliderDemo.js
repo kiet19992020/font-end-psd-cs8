b3188192b6884a7ff5f958d3f24828450cb67218
DESKTOP-J67BSCC\DELL <kietnguyen1147@gmail.com>
2021-03-17 23:34:03 +0700

    update

--- src/assets/javascripts/modules/SliderDemo.js
+++ src/assets/javascripts/modules/SliderDemo.js
@@ -11,30 +11,23 @@ const internals = {
       rows: 0,
       slidesToShow: 4,
       slidesToScroll: 1,
-      adaptiveHeight: true,
       prevArrow: '<button class="rounded-circle slide-btn-left icomoon icon-arrow-thin-left"></button>',
       nextArrow: '<button class="rounded-circle slide-btn-right icomoon icon-arrow-thin-right"></button>',
-      centerPadding: '50px',
       speed: 300,
-      // centerMode: true,
       responsive: [
         {
           breakpoint: 1024,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 2,
-            adaptiveHeight: true,
-            centerPadding: '50vw',
-            variableWidth: true,
-            respondTo: 'container'
+            slidesToScroll: 3,
+            infinite: true
           }
         },
         {
           breakpoint: 600,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 2,
-            adaptiveHeight: true
+            slidesToScroll: 2
           }
         },
         {
@@ -44,9 +37,6 @@ const internals = {
             slidesToScroll: 1
           }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
       ]
     })
   }
