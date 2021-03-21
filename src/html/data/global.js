331f7908a77d34c18ac59e244ed75cb2f3814d45
DESKTOP-J67BSCC\DELL <kietnguyen1147@gmail.com>
2021-03-21 21:03:27 +0700

    update nunjuck

--- src/html/data/global.js
+++ src/html/data/global.js
@@ -3,6 +3,7 @@ module.exports = {
   banner: require('./modules/banner1'),
   slides: require('./modules/slide'),
   contentds: require('./modules/contentds'),
+  abouts: require('./modules/abouts'),
   itemsicon: require('./modules/contenticon'),
   imagesBase64: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC',
   interJs: 'app.js'
---
+++ src/html/data/modules/abouts.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    title: 'Case Study Headline can go to two lines',
+    bg: '#1c9bd6'
+  },
+  {
+    title: 'Case Study Headline can go to two lines',
+    bg: '#62bae3'
+  },
+  {
+    title: 'Case Study Headline can go to two lines',
+    bg: '#a6d8f0'
+  },
+  {
+    title: 'Case Study Headline can go to two lines',
+    bg: '#ceeaf7'
+  }
+]
