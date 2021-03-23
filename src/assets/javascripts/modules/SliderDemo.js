cd9868cbfee364473f47c30ea321333deb4730f9
DESKTOP-J67BSCC\DELL <kietnguyen1147@gmail.com>
2021-03-23 20:39:43 +0700

    update 23/3/2021

--- src/assets/javascripts/modules/SliderDemo.js
+++ src/assets/javascripts/modules/SliderDemo.js
@@ -20,7 +20,7 @@ const internals = {
           breakpoint: 1500,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToScroll: 2,
             infinite: true
           }
         },
